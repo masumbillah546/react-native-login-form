758d821884bf83b4e3c164d2aa1a9164aeb1b974
Masum Billah <masumbillah546@gmail.com>
2021-02-06 23:00:55 +0600

    login form update

--- App.js
+++ App.js
@@ -1,36 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
+    <React.Fragment>
     <View style={styles.container}>
       <Text style={{fontSize:50}}>Login</Text>
       <Text>Welcome back,</Text>
       <Text>Signin to cotinue etiam tecimates sed ad</Text>
       <TextInput
-        style={{height: 40}}
+        style={{
+          height: 40,
+           width:350,
+        }}
         placeholder="Email"
         defaultValue={''}
       />
       <TextInput
-        style={{height: 40}}
+        style={{
+          height: 40,
+          width:350,
+         }}
         placeholder="Password"
         
         defaultValue={''}
       />
+       <Text>Forgot password?</Text>
       <StatusBar style="auto" />
     </View>
+    <View style={styles.container2}>
+      
+     <Button
+        title="Sign in"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+     
+    </View>
+    </React.Fragment>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: '#fff',
-    // backgroundColor: '#253B3B',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+   container2: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+
+
+  },
+  
 
 });
