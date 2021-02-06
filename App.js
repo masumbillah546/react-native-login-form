989ac085de3eadf4c84faea04c788abce0f0dbd8
Masum Billah <masumbillah546@gmail.com>
2021-02-06 19:19:28 +0600

    login form

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize:50}}>Login</Text>
+      <Text>Welcome back,</Text>
+      <Text>Signin to cotinue etiam tecimates sed ad</Text>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Email"
+        defaultValue={''}
+      />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Password"
+        
+        defaultValue={''}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +28,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    // backgroundColor: '#253B3B',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
